be55be3c18097cb89541b5a868dbe8c857b5e698
Lokesh Kumar <lokeshkumar@Lokeshs-MacBook-Pro.local>
2024-06-19 18:26:46 +0530

    loading animation added

--- app/Components/Home.tsx
+++ app/Components/Home.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, ScrollView } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 import Navbar from "../Components/Navbar";
 import Slider from "../Components/Slider";
 import QuickDel from "../Components/QuickDel";
@@ -8,9 +8,34 @@ import OurMeal from "../Components/OurMeal";
 import SpecialPlatters from "../Components/SpecialPlatters";
 import TopCategories from "../Components/TopCategories";
 import Footer from "../Components/Footer";
+
 const Home = ({ navigation }: any) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 4500);
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <Image
+          source={{
+            uri: "https://i.pinimg.com/originals/ee/1d/08/ee1d081c5bdf966b058c1a6588e73e8a.gif",
+          }}
+          style={styles.image}
+        />
+        <Text style={styles.title}>
+          Bring<Text style={styles.highlighted}>Meals</Text>
+        </Text>
+      </View>
+    );
+  }
+
   return (
-    <ScrollView>
+    <ScrollView style={styles.container}>
       <Navbar nav={navigation} />
       <Slider />
       <QuickDel />
@@ -23,6 +48,30 @@ const Home = ({ navigation }: any) => {
   );
 };
 
-export default Home;
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fffafc",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fffafc",
+  },
+  image: {
+    width: 300,
+    height: 300,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#000000",
+  },
+  highlighted: {
+    color: "#C2410D",
+  },
+});
 
-const styles = StyleSheet.create({});
+export default Home;
